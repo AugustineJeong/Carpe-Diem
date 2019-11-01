4fd3031332838caad6f5d1036da9ad22b6602d21
augusin11 <augusin11@gmail.com>
2019-11-01 14:51:09 -0700

    v7.6 (complete)

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        //instantiation of implementation
         UserInterface program1 = new ProgramVersion1();
         program1.execute();
     }
